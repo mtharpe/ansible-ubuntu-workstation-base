2dbf9b219d43eedb8602fbe2acffb46ee84ab7f7
Michael Tharpe <mtharpe@gmail.com>
2025-03-02 15:39:19 -0500

    Add Ansible playbook tasks for additional apt repositories and package management

---
+++ wsl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ansible=`which ansible`
+
+if [ -z "$ansible" ]; then
+ sudo apt install ansible -y
+fi
+
+if [ ! -f "/etc/sudoers/${USER}" ]; then
+  echo "${USER} ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/${USER}
+fi
+
+if [ -z "$ansible" ]; then
+ sudo dnf install ansible -y
+fi
+
+until ansible-playbook --extra-vars "local_user=${USER}" setup_wsl.yml; do
+  echo Ansible run disrupted, retrying in 10 seconds...
+  sleep 10
+done
+
+sudo rm -f /etc/sudoers.d/${USER}
